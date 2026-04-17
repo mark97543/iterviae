a006acfa465d052af68494bccaed02b50ea957a4
Mark Wade <wade.mark.a@gmail.com>
2026-04-17 11:14:04 -0600

    Enter while focused on the input now executes the search function

--- src/assets/PrivatePages/Main/Parts/Left Panel/Parts/LeftPanelSearch.tsx
+++ src/assets/PrivatePages/Main/Parts/Left Panel/Parts/LeftPanelSearch.tsx
@@ -28,6 +28,11 @@ const LeftPanelSearch = () => {
     const [search, setSearch] = useState("");
     const {setSearchStop} = useStops();
 
+    //Enter Key event
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') handleSearch(); 
+    };
+
     const handleSearch = () =>{
         //Break the long/lat up based on comma and convert to numbers
         const coords = search.split(',');
@@ -39,7 +44,15 @@ const LeftPanelSearch = () => {
     return(        
         <div className="left-panel-search-wrapper">
             <style>{LEFT_PANEL_SEARCH_STYLE}</style>
-            <input className="std-input" type="text" onFocus={(e) => e.target.select()} placeholder="Search for location" value={search} onChange={(e) => setSearch(e.target.value)} /> 
+            <input 
+                className="std-input" 
+                type="text" 
+                onFocus={(e) => e.target.select()} 
+                placeholder="Search for location" 
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={handleKeyDown}    
+            /> 
             <button className="std-button panel-search-button" onClick={handleSearch}><img src="./search.png" alt="Search" /></button>
         </div>     
     )
